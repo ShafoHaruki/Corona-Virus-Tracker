9974fb80c543a55c828fe87160747db1ed25bab2
test <muhammadsyafizuraimi@gmail.com>
2020-06-04 03:05:20 +0800

    first commit eva

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api";
+import styles from "./App.module.css";
+
+// import image from './images/image.png';
+
+class App extends React.Component {
+  //don't have to include constructor, as JS takes care of it in the background + it's cleaner syntax too
+  state = {
+    data: {},
+    country: "",
+  };
+  //componentDidMount is a hook that gets invoked right after a component has been mounted, aka after the first render
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  render() {
+    return (
+      //we put style.container to make sure we don't have inteferance with other CSS files
+      <div className={styles.container}>
+        <h1>App</h1>
+        <Cards data={this.state.data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
